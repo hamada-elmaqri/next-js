eacfa9163dd5d9d7ee6e55489a3346112bea8d97
hamada-elmaqri <elmaqrih01@gmail.com>
2024-11-11 16:00:31 +0100

    "next-03" /student

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,11 +1,21 @@
-import Tableau from "@/components/tableau"
+import EventCalendar from "@/components/EventCalendar";
+import Announcement from "@/components/Announcement";
+import {BigCalendar} from "@/components/BigCalendar";
 
 const StudentPage = () => {
-  return (
-    <div className=''>
-      <p>hey  </p>
-    </div>
-  )
+    return (
+        <div className='p-4 flex flex-col xl:flex-row gap-8'>
+            {/*left*/}
+            <div className='w-full xl:w-2/3'>
+                <div className='text-xl font-semibold  '>Schedule(4A)</div>
+                <div><BigCalendar/></div>
+            </div>
+            {/*RIGHT*/}
+            <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+                <EventCalendar/>
+                <Announcement/>
+            </div>
+        </div>
+    );
 }
-
 export default StudentPage
