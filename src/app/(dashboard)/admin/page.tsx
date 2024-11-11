1eae100784a4ad4bb959017a21adca3589865690
hamada-elmaqri <elmaqrih01@gmail.com>
2024-11-11 10:16:45 +0100

    "next-01" admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -6,39 +6,39 @@ import EventCalendar from "@/components/EventCalendar";
 import Announcement from "@/components/Announcement";
 
 const AdminPage = () => {
-  return (
-    <div className="p-4 flex flex-col md:flex-row gap-4">
-      {/* left */}
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* user card */}
-        <div className="flex flex-wrap justify-between gap-4">
-          <UserCard type="student" />
-          <UserCard type="teacher" />
-          <UserCard type="parent" />
-          <UserCard type="admin" />
+    return (
+        <div className="p-4 flex flex-col md:flex-row gap-4">
+            {/* left */}
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                {/* user card */}
+                <div className="flex flex-wrap justify-between gap-4">
+                    <UserCard type="student"/>
+                    <UserCard type="teacher"/>
+                    <UserCard type="parent"/>
+                    <UserCard type="admin"/>
+                </div>
+                {/* middleChart */}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/* countChart */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart/>
+                    </div>
+                    {/* ATTENDANCE CHART*/}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart/>
+                    </div>
+                </div>
+                {/* buttonChart */}
+                <div className="w-full h-[500px]">
+                    <FinanceChart/>
+                </div>
+            </div>
+            {/* right */}
+            <div className="w-full lg:w-1/3 gap-8 flex flex-col justify-between">
+                <EventCalendar/>
+                <Announcement/>
+            </div>
         </div>
-        {/* middleChart */}
-        <div className="flex gap-4 flex-col lg:flex-row">
-          {/* countChart */}
-          <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart />
-          </div>
-          {/* ATTENDANCE CHART*/}
-          <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
-          </div>
-        </div>
-        {/* buttonChart */}
-        <div className="w-full h-[500px]">
-          <FinanceChart />
-        </div>
-      </div>
-      {/* right */}
-      <div className="w-full lg:w-1/3 gap-8 flex flex-col justify-between">
-            <EventCalendar/>
-            <Announcement/>
-      </div>
-    </div>
-  )
+    )
 }
 export default AdminPage
